c2783ce4f56310260fc113756440c7cfdc6210ca
Dan Park <danieljinwoopark@gmail.com>
2018-03-21 15:55:58 -0500

    working config.json

--- url_scrape.py
+++ url_scrape.py
@@ -1,80 +1,16 @@
-import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+
 import json
+import pandas as pd
 
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
-#https://www.pythonsheets.com/notes/python-sqlalchemy.html
-
-
-"""
-if_exists : {'fail', 'replace', 'append'}, default 'fail'
-    - fail: If table exists, do nothing.
-    - replace: If table exists, drop it, recreate it, and insert data.
-    - append: If table exists, insert data. Create if does not exist.
-"""
+def db_connection():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return create_engine(URL(**config))
 
 
+engine = db_connection()
+df = pd.read_sql("SELECT * FROM test_table", engine)
 
-
-
-
-
-
-
-
-
-
-
-
-
-import requests
-import re
-import json
-
-tag = 'cavapoo'
-url_string = "https://www.instagram.com/explore/tags/%s/?__a=1" % tag
-
-req = requests.get(url_string).json()
-d = req['graphql']['hashtag']['edge_hashtag_to_media']
-
-print(len(d['edges']))
-
-with open('file.txt', 'w') as file:
-    # file.write(soup.prettify())
-    file.write(json.dumps(req))
-
-for eachNode in d['edges']:
-    comment = eachNode['node']['edge_media_to_caption']['edges'][0]['node']['text']
-    taken_at_timestamp = eachNode['node']['taken_at_timestamp']
-    height = eachNode['node']['dimensions']['height']
-    width = eachNode['node']['dimensions']['width']
-    display_url = eachNode['node']['display_url']
-    likes = eachNode['node']['edge_liked_by']
-    is_video = eachNode['node']['is_video']
-    owner = eachNode['node']['owner']['id']
-    print(owner)
-
-
-# data = json.loads(however_youre_getting_the_data('https://www.instagram.com/explore/tags/plebiscito/?__a=1&max_id={}'.format(end_cursors[-1])))
-
-
-## write a function using the shortcode of an image and looking up the username
-# https://www.instagram.com/p/{SHORTCODE}/?__a=1
-# https://www.instagram.com/smena8m/?__a=1 <-- if you have the UID
-
-## maybe i'll make a postgres database to put it in
-
-# then i need to update it once in a while with all users and number of followers
-
-
-conn = psycopg2.connect(dbname='instagram',
-                        user='dan.park',
-                        host='localhost',
-                        password = 'da010201')
-# option 1
-cursor = conn.cursor()
-
-
-#option 2
-pd.read_sql_query(query, conn)
+print(df)
